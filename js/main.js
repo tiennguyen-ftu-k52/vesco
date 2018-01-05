0c7f41122f27c69d8b1c931aa6aee8fdc6721059
Tien Nguyen <tiennguyen.ftu.k52@gmail.com>
2018-01-05 21:49:48 +0700

    finish navigation section

--- js/main.js
+++ js/main.js
@@ -1,69 +1,108 @@
 /*  ===========================
           SERVICES
 =========================== */
-$(function () {
+$(function() {
   new WOW().init();
 });
 
 /*  ===========================
           WORK
 =========================== */
-$(function () {
+$(function() {
   $('#work').magnificPopup({
     delegate: 'a',
     type: 'image',
     gallery: {
-      enabled: true
-    }
+      enabled: true,
+    },
   });
 });
 
 /*  ===========================
           TEAM
 =========================== */
-$(function () {
+$(function() {
   $('.team__members').owlCarousel({
     items: 3,
     autoplay: true,
     smartSpeed: 500,
     loop: true,
-    autoplayHoverPause: true
+    autoplayHoverPause: true,
   });
 });
 
 /*  ===========================
           TESTIMONIALS
 =========================== */
-$(function () {
+$(function() {
   $('.testimonials__customers').owlCarousel({
     items: 1,
     autoplay: true,
     smartSpeed: 700,
     loop: true,
-    autoplayHoverPause: true
+    autoplayHoverPause: true,
   });
 });
 
 /*  ===========================
           STATS
 =========================== */
-$(function () {
+$(function() {
   $('.counter').counterUp({
     delay: 10,
     time: 2000,
-    beginAt: 300
+    beginAt: 300,
   });
 });
 
 /*  ===========================
           CLIENTS
 =========================== */
-$(function () {
+$(function() {
   $('.clients__list').owlCarousel({
     items: 6,
     autoplay: true,
     smartSpeed: 700,
     loop: true,
-    autoplayHoverPause: true
+    autoplayHoverPause: true,
+  });
+});
+
+/*  ===========================
+          NAVIGATION
+=========================== */
+// Show/hide nav bar
+$(function() {
+  $(window).scroll(function() {
+    if ($(this).scrollTop() < 50) {
+      // hide nav
+      $('.vesco-navbar.navbar').removeClass('scrollable');
+      $('.btn--back-to-top').fadeOut();
+    } else {
+      // show nav
+      $('.vesco-navbar.navbar').addClass('scrollable');
+      $('.btn--back-to-top').fadeIn();
+    }
+  });
+});
+
+// Smooth scrolling
+$(function() {
+  $('a.smooth-scrolling').click(function(event) {
+    event.preventDefault();
+
+    // get section id
+    var section = $(this).attr('href');
+
+    $('html, body').animate(
+      {
+        scrollTop: $(section).offset().top - 64,
+      },
+      750,
+      'easeInOutExpo',
+      function () {
+        window.location.hash = section;
+      }
+    );
   });
 });
